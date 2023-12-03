f4902f195f6853c88353fde08716d30386ca63d7
dasUnicorn <info@dasunicorn.de>
2023-12-03 16:02:42 +0100

    add main function to the program

--- run.py
+++ run.py
@@ -1,3 +1,18 @@
 # Your code goes here.
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
+
+def welcome():
+	print("Welcome to the game!")
+
+
+def main():
+    """
+    Run the game.
+    """
+    welcome()
+
+
+# Ensures main is only executed when the script is directly run
+if __name__ == "__main__":
+    main()
