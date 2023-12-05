2ed47f67f1099ac0f92bd5913886e651c1146bcb
dasUnicorn <info@dasunicorn.de>
2023-12-05 12:42:49 +0100

    add delete() function to delete the players score

--- run.py
+++ run.py
@@ -28,6 +28,7 @@ def welcome():
     print("You will habe to make decisions that effect your story and takes you on different paths.")
     print("Try to find as many ending as possible.")
     print("You have found " + str(found_endings) + " of " + str(number_endings) + " endings so far.")
+    print("-------------------------------------")
 
 
 def get_endings():
@@ -46,13 +47,43 @@ def get_endings():
     
     return found_endings, number_endings
 
+def delete():
+    """
+    Delete the current score by setting all endings to false.
+    """
+    print("...deleting your score...")
+    data = endings.get_all_values()
+    i = 1
+
+    for item in data:
+        endings.update_cell(i, 2, 'FALSE')
+        i += 1
+
+    print("Your score has been deleted.")
+
+
+def play():
+    print("Let's play!")
+
 
 def main():
     """
     Base function running the game.
     """
     welcome()
-    login()
+    print("Type 'start', to start the game with your current score.")
+    print("Type 'delete', to delete your score and start from scratch.")
+    game = input()
+    if game == "delete":
+        print("Are you sure you want to delete your current score?")
+        delete_check = input("[Type 'YES' to delete your score:]")
+        if delete_check == "YES":
+            delete()
+        else:
+            print("Your score has NOT been deleted.")
+    elif game == "start":
+        play()
+
 
 
 # Making sure the script is run directly
