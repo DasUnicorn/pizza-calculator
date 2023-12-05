3fd3179fd8295386a830e515d33d819c8142ba7e
dasUnicorn <info@dasunicorn.de>
2023-12-05 10:02:46 +0100

    update welcome text when the games starts

--- run.py
+++ run.py
@@ -3,10 +3,20 @@
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
 
 def welcome():
-	"""
-	Printing the Welcome message at the beginning of each game on the screen
-	"""
-	print("Welcome to the game!")
+    """
+    Printing the Welcome message at the beginning of each game on the screen
+    """
+    print("                    _       __                       _                           ")
+    print("                   | |     / _|                     | |                          ")
+    print("__      _____  _ __| | __ | |_ _ __ ___  _ __ ___   | |__   ___  _ __ ___   ___  ")
+    print("\ \ /\ / / _ \| '__| |/ / |  _| '__/ _ \| '_ ` _ \  | '_ \ / _ \| '_ ` _ \ / _ \ ")
+    print(" \ V  V / (_) | |  |   <  | | | | | (_) | | | | | | | | | | (_) | | | | | |  __/ ")
+    print("  \_/\_/ \___/|_|  |_|\_\ |_| |_|  \___/|_| |_| |_| |_| |_|\___/|_| |_| |_|\___| ")
+    print("---------------------------------------------------------------------------------")
+    print("")
+    print("Welcome to 'work from home' a story telling game that takes you on a journey in your virtual home.")
+    print("You will habe to make decisions that effect your story and takes you on different paths.")
+    print("Try to find as many ending as possible.")
 
 
 def main():
